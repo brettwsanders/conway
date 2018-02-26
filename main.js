0620fa1e164e430751fe46c7af62d81d8d15bc6e
Brett Sanders <brett.sanders@vevo.com>
2018-02-25 16:32:40 -0800

    initialize Game board

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-(function() {}());
+(function() {
+    const $ = selector => document.querySelector(selector);
+
+    function Game(size) {
+        this.size = size;
+        this.board = this.initializeBoard(size);
+    };
+
+    Game.prototype.initializeBoard = function() {
+        return new Array(this.size)
+            .fill([])
+            .map(i => new Array(this.size).fill(0));
+    };
+
+    Game.prototype.toggle = function(x, y) {
+        this.board[y][x] = +!this.board[y][x];
+    }
+
+    const game = new Game(5);
+
+    game.toggle(2, 1);
+    game.toggle(2, 2);
+    game.toggle(2, 3);
+
+    console.log(game.board.join("\n"));
+}());
