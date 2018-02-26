d413fe146ec290e35b2b73b88421e27f7c8a01e6
Brett Sanders <brett.sanders@vevo.com>
2018-02-25 17:11:33 -0800

    implement next

--- main.js
+++ main.js
@@ -14,13 +14,45 @@
 
     Game.prototype.toggle = function(x, y) {
         this.board[y][x] = +!this.board[y][x];
-    }
+    };
+
+    Game.prototype.next = function() {
+        const prevBoard = this.board.slice().map(row => row.slice());
+        for (let y = 0; y < this.size; y++) {
+            for (let x = 0; x < this.size; x++) {
+                const alive = prevBoard[y][x];
+                const neighbors = getNeighbors(prevBoard, x, y);
+                // if live and less than 2 neighbors, die
+                if (alive && neighbors < 2) {
+                    this.toggle(x, y);
+                // if live and more than 3 neighbors, die
+                } else if (alive && neighbors > 3) {
+                    this.toggle(x, y);
+                // if dead and 3 live neighbors, come alive
+                } else if (!alive && neighbors === 3) {
+                    this.toggle(x, y);
+                }
+            }
+        }
+    };
+
+    // helpers
+    const getNeighbors = function(board, x, y) {
+        let sum = 0;
+        if (board[y-1]) {
+            sum += (+!!board[y-1][x-1] + board[y-1][x] + +!!board[y-1][x+1]);
+        }
+        if (board[y+1]) {
+            sum += (+!!board[y+1][x-1] + board[y+1][x] + +!!board[y+1][x+1]);
+        }
+        sum += (+!!board[y][x-1] + +!!board[y][x+1]);
+        return sum;
+    };
+
 
     const game = new Game(5);
 
-    game.toggle(2, 1);
-    game.toggle(2, 2);
-    game.toggle(2, 3);
-
     console.log(game.board.join("\n"));
 }());
+
+
